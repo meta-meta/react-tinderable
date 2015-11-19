856234bc2e9db7ea2fa53365012afeea6b06491a
Paul M. Christian <paulmchristian@gmail.com>
2015-11-19 15:54:02 -0600

    decouple images directory from tinderable component

--- examples/js/index.jsx
+++ examples/js/index.jsx
@@ -6,25 +6,25 @@ var cardsData = [
     {
         title: 'A wonderful day',
         text: '—— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— -',
-        image: 'portrait-1.jpg',
+        image: 'images/portrait-1.jpg',
         id: '1'
     },
     {
         title: 'My amazing journey',
         text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ——— ———— ',
-        image: 'portrait-2.jpg',
+        image: 'images/portrait-2.jpg',
         id: '2'
     },
     {
         title: 'Three recipes without cocoa',
         text: ' - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ——— - - - ———',
-        image: 'portrait-3.jpg',
+        image: 'images/portrait-3.jpg',
         id: '3'
     },
     {
         title: 'Generic clickbait title',
         text: ' —— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— —— - ——— - - - ——— ———— - — ——— —— - ————— - - ———— - ——— ',
-        image: 'portrait-4.jpg',
+        image: 'images/portrait-4.jpg',
         id: '4'
     }
 ];
